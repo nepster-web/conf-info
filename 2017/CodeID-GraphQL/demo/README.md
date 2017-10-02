8fa7fa58dfa520bc7c6b0073cb22507eca07c528
Nepster <nepsterxxx@mail.ru>
2017-10-02 20:08:10 +0300

    fix GraphQL info

---
+++ 2017/CodeID-GraphQL/demo/README.md
@@ -0,0 +1,22 @@
+# Запуск DEMO
+
+Установка необходимых расширений с помощью [composer](https://getcomposer.org/):
+```
+composer install
+```
+
+Запуск development сервера:
+```
+ php -S localhost:8000 simple1.php
+```
+
+Просмотр в браузере:
+```
+ http://localhost/simple1.php?query=query{echo(message:"Hello World")}
+```
+
+Подробная информация указана в виде комментариев в файлах: simple1.php и simple2.php.
+
+----------
+
+*Если Вы обнаружили ошибку, пожалуйста сообщите мне об этом.*
--- 2017/CodeID-GraphQL/demo/simple2.php
+++ 2017/CodeID-GraphQL/demo/simple2.php
@@ -1,262 +1,211 @@
 <?php
 
+/**
+ * Если нужно, запускаем сервер
+ * php -S localhost:8000 simple2.php
+ *
+ * Пример запросов с помощью браузера
+ * GET http://localhost:8000/simple2.php?query=query($first: Int!){allUsers(first: $first){nodes{id,name,position}}}&variables={"first": "7"}
+ * GET http://localhost:8000/simple2.php?query=query($id: Int!){User(id: $id){id,name,position}}&variables={"id": "7"}
+ *
+ * POST http://localhost:8000/simple2.php
+ * query=mutation ($id: Int!, $position: String!) {
+ *   updateUser(id: $id, position: $position) {
+ *     User {
+ *       id,
+ *       name,
+ *       position
+ *     }
+ *     result {
+ *       approval,
+ *       errors {
+ *       field,
+ *         message
+ *       }
+ *     }
+ *   }
+ * }&variables={"id": "17", "position": "new Position"}
+ */
+
 require(__DIR__ . '/vendor/autoload.php');
 
+use GraphQL\Schema;
 use GraphQL\GraphQL;
 use GraphQL\Type\Definition\Type;
-use GraphQL\Schema as GraphQLSchema;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\InterfaceType;
-use GraphQL\Type\Definition\InputObjectType;
-
-/**
- * Class Controller
- */
-class Controller
-{
-
-    /**
-     * @var array
-     */
-    private $objectTypes = [];
-
-    /**
-     *
-     */
-    public function entryPoint()
-    {
-        $query = '
-            query($first: Int!) { 
-                allUsers(first: $first) {
-                    nodes {
-                        id,
-                        name,
-                        position,
-                    }
-                } 
-            }';
-        $variables = ['first' => 10];
 
 
-        $query = '
-            query($id: Int!) { 
-                User(id: $id) {
-                    id,
-                    name,
-                    position,
-                } 
-            }';
-        $variables = ['id' => 7];
-
-        ////////////////////////////////////////////
-
-        $result = GraphQL::executeAndReturnResult(
-            $this->schema(),
-            $query,
-            [],
-            null,
-            $variables
-        );
-        $output = $result->toArray();
-
-        echo json_encode($output);
-    }
+// ------------------------------------------
+if (empty($_POST)) {
+    $query = isset($_GET['query']) ? $_GET['query'] : null;
+    $variables = isset($_GET['variables']) ? json_decode($_GET['variables'], true) : null;
+} else {
+    $query = isset($_POST['query']) ? $_POST['query'] : null;
+    $variables = isset($_POST['variables']) ? json_decode($_POST['variables'], true) : null;
+}
+// ------------------------------------------
 
 
+// ------------------------------------------
+$userInterfaceType = new InterfaceType([
+    'name' => 'UserInterface',
+    'fields' => function () use (&$interface) {
+        return [
+            'id' => Type::nonNull(Type::string()),
+            'name' => Type::nonNull(Type::string()),
+            'position' => Type::nonNull(Type::string()),
+        ];
+    },
+    'resolveType' => function ($obj) use (&$userObjectType) {
+        return $userObjectType;
+    },
+]);
+
+$userObjectType = new ObjectType([
+    'name' => 'UserObjectType',
+    'fields' => [
+        'id' => Type::nonNull(Type::string()),
+        'name' => Type::nonNull(Type::string()),
+        'position' => Type::nonNull(Type::string()),
+    ],
+    'interfaces' => [$userInterfaceType]
+]);
+// ------------------------------------------
 
 
+// ------------------------------------------
+$queryAllUsers = [
+    'type' => new ObjectType([
+        'name' => 'allUsers',
+        'fields' => [
+            'nodes' => Type::listOf($userInterfaceType),
+        ],
+    ]),
+    'args' => [
+        'first' => Type::nonNull(Type::int()),
+    ],
+    'resolve' => function (array $rootValue, array $args, $context, ResolveInfo $info) {
+        $result = [];
+        for ($i = 1; $i <= 100; ++$i) {
+            $result[] = [
+                'id' => $i,
+                'name' => 'User-' . $i,
+                'position' => 'pos-' . $i,
+            ];
+        }
+        $result = array_slice($result, 0, $args['first'], true);
+        return ['nodes' => $result];
+    },
+];
 
-    private function schema()
-    {
-        $this->registerUsers();
-
-        return new GraphQLSchema([
-            'query' => new ObjectType([
-                'name' => 'Query',
-                'fields' => [
-                    'allUsers' => $this->queryAllUsers(),
-                    'User' => $this->queryUser(),
-                ],
-            ]),
-            'mutation' => new ObjectType([
-                'name' => 'Mutation',
-                'fields' => [
-                    //'updateUser' => $this->mutationUpdateUser(),
-                ],
-            ]),
-            'types' => [
-                $this->objectTypes['UserObjectType'],
-            ],
-        ]);
-    }
-
-    /**
-     * Init type of User
-     */
-    private function registerUsers()
-    {
-        $this->objectTypes['UserInterface'] = new InterfaceType([
-            'name' => 'UserInterface',
-            'fields' => function () use (&$interface) {
+$queryUser = [
+    'type' => $userInterfaceType,
+    'args' => [
+        'id' => Type::nonNull(Type::int()),
+    ],
+    'resolve' => function (array $rootValue, array $args, $context, ResolveInfo $info) {
+        for ($i = 1; $i <= 100; ++$i) {
+            if ((int)$args['id'] === $i) {
                 return [
-                    'id' => Type::nonNull(Type::string()),
-                    'name' => Type::nonNull(Type::string()),
-                    'position' => Type::nonNull(Type::string()),
+                    'id' => $i,
+                    'name' => 'User-' . $i,
+                    'position' => 'pos-' . $i,
                 ];
-            },
-            'resolveType' => function ($obj) {
-                return $this->objectTypes['UserObjectType'];
-            },
-        ]);
+            }
+        }
+        return null;
+    },
+];
 
-        $this->objectTypes['UserObjectType'] = new ObjectType([
-            'name' => 'UserObjectType',
-            'fields' => [
-                'id' => Type::nonNull(Type::string()),
-                'name' => Type::nonNull(Type::string()),
-                'position' => Type::nonNull(Type::string()),
+$mutationUpdateUser = [
+    'type' => new ObjectType([
+        'name' => 'updateUser',
+        'fields' => [
+            'User' => $userInterfaceType,
+            'result' => [
+                'type' => (new ObjectType([
+                    'name' => 'UpdateUserResultObjectType',
+                    'fields' => [
+                        'approval' => Type::boolean(),
+                        'errors' => Type::listOf(
+                            (new ObjectType([
+                                'name' => 'UpdateUserErrorObjectType',
+                                'fields' => [
+                                    'field' => Type::string(),
+                                    'message' => Type::string(),
+                                ],
+                            ]))
+                        ),
+                    ]
+                ]))
             ],
-            'interfaces' => [$this->objectTypes['UserInterface']]
-        ]);
-    }
+        ],
+    ]),
+    'args' => [
+        'id' => Type::nonNull(Type::int()),
+        'position' => Type::nonNull(Type::string()),
+    ],
+    'resolve' => (function ($entity, array $args, $context, ResolveInfo $info) {
+        $fields = $info->getFieldSelection(2);
 
-    /**
-     * @return array
-     */
-    private function queryAllUsers()
-    {
-        return [
-            'type' => new ObjectType([
-                'name' => 'AllUsers',
-                'fields' => [
-                    'nodes' => Type::listOf($this->objectTypes['UserInterface']),
-                ],
-            ]),
-            'args' => [
-                'first' => Type::nonNull(Type::int()),
-            ],
-            'resolve' => function (array $rootValue, array $args, $context, ResolveInfo $info) {
-                $result = [];
-                for ($i = 1; $i <= 100; ++$i) {
-                    $result[] = [
-                        'id' => $i,
-                        'name' => 'User-' . $i,
-                        'position' => 'pos-' . $i,
-                    ];
-                }
-                $result = array_slice($result, 0, $args['first'], true);
-                return ['nodes' => $result];
-            },
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    private function queryUser()
-    {
-        return [
-            'type' => $this->objectTypes['UserInterface'],
-            'args' => [
-                'id' => Type::nonNull(Type::int()),
-            ],
-            'resolve' => function (array $rootValue, array $args, $context, ResolveInfo $info) {
-                for ($i = 1; $i <= 100; ++$i) {
-                    if ((int)$args['id'] === $i) {
-                        return [
-                            'id' => $i,
-                            'name' => 'User-' . $i,
-                            'position' => 'pos-' . $i,
-                        ];
-                    }
-                }
-                return null;
-            },
-        ];
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     * Update setting
-     * @return array
-     */
-    private function mutationUpdateSetting()
-    {
-        return [
-            'type' => new ObjectType([
-                'name' => 'updateSettingObjectType',
-                'fields' => [
-                    'Setting' => $this->objectTypes['SettingsObjectType'],
-                    'result' => $this->getGraphQLResultObjectType('updateSetting'),
-                ],
-            ]),
-            'args' => [
-                'param' => Type::nonNull(Type::string()),
-                'type' => Type::string(),
-                'value' => Type::string(),
-                'description' => Type::string(),
-            ],
-            'resolve' => (function (array $rootValue, array $args, $context, ResolveInfo $info) {
-
-                echo 3;
-                die();
-            })
-        ];
-    }
-
-
-    /**
-     * @param string $prefix
-     * @return array
-     */
-    private static function getGraphQLResultObjectType($prefix = '')
-    {
-        $prefix = ($prefix) ?: uniqid(true);
+        // TODO Логика изменений данных пользователя
 
         return [
-            'type' => (new ObjectType([
-                'name' => $prefix . 'ResultObjectType',
-                'fields' => [
-                    'approval' => Type::boolean(),
-                    'errors' => Type::listOf(
-                        (new ObjectType([
-                            'name' => $prefix . 'ErrorObjectType',
-                            'fields' => [
-                                'field' => Type::string(),
-                                'message' => Type::string(),
-                            ],
-                        ]))
-                    ),
-                ]
-            ]))
+            'User' => [
+                'id' => $args['id'],
+                'name' => 'User-' . $args['id'],
+                'position' => $args['position'],
+            ],
+            'result' => [
+                'approval' => true,
+                'errors' => null,
+            ]
         ];
-    }
+    })
+];
+// ------------------------------------------
 
+
+// ------------------------------------------
+$schema = new Schema([
+    'query' => new ObjectType([
+        'name' => 'Query',
+        'fields' => [
+            'allUsers' => $queryAllUsers,
+            'User' => $queryUser,
+        ],
+    ]),
+    'mutation' => new ObjectType([
+        'name' => 'Mutation',
+        'fields' => [
+            'updateUser' => $mutationUpdateUser,
+        ],
+    ]),
+    'types' => [
+        $userObjectType,
+    ],
+]);
+// ------------------------------------------
+
+
+// ------------------------------------------
+try {
+    $rootValue = ['prefix' => 'You said: '];
+    $result = GraphQL::executeAndReturnResult($schema, $query, $rootValue, null, $variables);
+    $output = $result->toArray();
+} catch (\Exception $e) {
+    $output = [
+        'errors' => [
+            [
+                'message' => $e->getMessage()
+            ]
+        ]
+    ];
 }
 
 
-//  header('Content-Type: application/json');
-(new Controller())->entryPoint();
+header('Content-Type: application/json');
+echo json_encode($output);
+// ------------------------------------------
